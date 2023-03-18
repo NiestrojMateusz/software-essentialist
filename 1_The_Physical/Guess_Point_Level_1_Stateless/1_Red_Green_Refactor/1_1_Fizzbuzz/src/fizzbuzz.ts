89b72b015a2371e8615b413217d93237d76ace93
m.niestroj <mateusz.niestroj@equiqo.com>
2023-03-18 16:55:52 +0100

    📦 NEW: fizzbuzz throws when input is bigger than max

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,5 +1,6 @@
 export const fizzbuzz = (num: number): string => {
   if (num < 1) throw new Error("Too small");
+  if (num > 100) throw new Error("Too large");
 
   return num.toString();
 };
--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -10,4 +10,8 @@ describe("fizzbuzz", () => {
   it("should doesnt accept numbers less than 1 such as -1", () => {
     expect(() => fizzbuzz(-1)).toThrow("Too small");
   });
+
+  it("should does not accept numbers over 100", () => {
+    expect(() => fizzbuzz(101)).toThrow("Too large");
+  });
 });
