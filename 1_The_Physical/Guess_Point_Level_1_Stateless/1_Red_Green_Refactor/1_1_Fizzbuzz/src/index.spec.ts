0c9c224fd19845d8871a8b3fa700777b640550a2
m.niestroj <mateusz.niestroj@equiqo.com>
2023-03-18 16:52:41 +0100

    📦 NEW: Add fizzbuzz test for string type return value

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -1,5 +1,9 @@
+import { fizzbuzz } from "./fizzbuzz";
 
 describe("fizzbuzz", () => {
+  it("should return a string", () => {
+    const result = fizzbuzz(1);
 
-
+    expect(typeof result).toBe("string");
+  });
 });
