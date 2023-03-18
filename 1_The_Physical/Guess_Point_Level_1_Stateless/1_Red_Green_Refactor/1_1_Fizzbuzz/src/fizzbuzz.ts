2c1c644899003334e0684bc92ff4fa4d7ac9fdba
m.niestroj <mateusz.niestroj@equiqo.com>
2023-03-18 17:31:40 +0100

    👌 IMPROVE: Extract modulus operation to own functions

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_1_Fizzbuzz/src/fizzbuzz.ts
@@ -1,16 +1,23 @@
+const isMultipleOf = (divisor: number) => (num: number) => {
+  return num % divisor === 0;
+};
+
+const isMultipleOf3 = isMultipleOf(3);
+const isMultipleOf5 = isMultipleOf(5);
+
 export const fizzbuzz = (num: number): string => {
   if (num < 1) throw new Error("Too small");
   if (num > 100) throw new Error("Too large");
 
-  if (num % 3 === 0 && num % 5 === 0) {
+  if (isMultipleOf3(num) && isMultipleOf5(num)) {
     return "fizzbuzz";
   }
 
-  if (num % 5 === 0) {
+  if (isMultipleOf5(num)) {
     return "buzz";
   }
 
-  if (num % 3 === 0) {
+  if (isMultipleOf3(num)) {
     return "fizz";
   }
 
