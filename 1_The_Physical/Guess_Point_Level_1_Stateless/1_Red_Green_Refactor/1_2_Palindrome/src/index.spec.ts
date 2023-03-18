8b231fdd8f33848a54e304ab4b073fc4dc8e1e1a
m.niestroj <mateusz.niestroj@equiqo.com>
2023-03-18 18:05:39 +0100

    📦 NEW: Tests to check when string is not a palindrome

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -4,4 +4,8 @@ describe('palindrome checker', () => {
     it.each(["mom", "wow"])('should be able to tell that "%s" is a palindrome', (word: string) => {
         expect(isPalindrome(word)).toBeTruthy();
     });
+
+    it.each(["bill", "essentialist"])('should be able to tell that "%s" is not a palindrome', (word: string) => {
+        expect(isPalindrome(word)).toBeFalsy();
+    });
 })
