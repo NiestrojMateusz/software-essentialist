66aa4035a12a17c5716f1a0b14343219238996d5
m.niestroj <mateusz.niestroj@equiqo.com>
2023-03-18 17:46:12 +0100

    📦 NEW: Working checks for basic palindromes

--- 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,7 @@
+import {isPalindrome} from "./index";
 
 describe('palindrome checker', () => {
-
+    it.each(["mom", "wow"])('should be able to tell that "%s" is a palindrome', (word: string) => {
+        expect(isPalindrome(word)).toBeTruthy();
+    });
 })
