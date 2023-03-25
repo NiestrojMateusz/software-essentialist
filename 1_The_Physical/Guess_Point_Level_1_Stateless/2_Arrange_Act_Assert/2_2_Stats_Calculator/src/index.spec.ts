893d6036232ebd8e6f23b9087a24b65400498bfc
m.niestroj <mateusz.niestroj@equiqo.com>
2023-03-25 18:39:07 +0100

    📦 NEW: Returning min value from sequence

--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.spec.ts
@@ -11,4 +11,27 @@ describe("Stats Calculator", () => {
     expect(result).toHaveProperty('elementsInSequence')
     expect(result).toHaveProperty('avg')
   })
+
+  it.each([
+      {
+        expected: -5,
+        numbers: [7, 12, -5, 0, 32, 18],
+      },
+      {
+        expected: -15,
+        numbers: [45, 23, -8, -15, 10, 5, 27],
+      },
+      {
+        expected: -10,
+        numbers: [15, -10, 30, 20, 9, -7, 42],
+      },
+    ]
+  )
+  ("should calculate the minimum value ($expected) of a sequence of integers $numbers", ({expected, numbers}) => {
+    const statsCalc = new StatsCalculator();
+
+    const result = statsCalc.calculate(numbers);
+
+    expect(result).toHaveProperty('minValue', expected)
+  });
 });
--- 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
+++ 1_The_Physical/Guess_Point_Level_1_Stateless/2_Arrange_Act_Assert/2_2_Stats_Calculator/src/index.ts
@@ -1,10 +1,14 @@
 export class StatsCalculator {
     calculate(input: number[]) {
         return {
-            minValue: null,
+            minValue: this.getMinValue(input),
             maxValue: null,
             elementsInSequence: null,
             avg: null
         }
+    }
+
+    private getMinValue(input: number[]) {
+        return input.sort((a, b) => a - b)[0]
     }
 }
